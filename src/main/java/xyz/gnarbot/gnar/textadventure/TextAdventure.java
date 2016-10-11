c33d95f3645575656f760f84a6b5407ebca568a3
Gatt <zacharygatt@me.com>
2016-10-11 20:48:39 +1100

    Fixed conflicts w/ Home Computer

--- src/main/java/xyz/gnarbot/gnar/textadventure/TextAdventure.java
+++ src/main/java/xyz/gnarbot/gnar/textadventure/TextAdventure.java
@@ -1,5 +1,6 @@
 package xyz.gnarbot.gnar.textadventure;
 
+<<<<<<< Updated upstream
 import net.dv8tion.jda.entities.Message;
 import net.dv8tion.jda.entities.User;
 import xyz.gnarbot.gnar.utils.Note;
@@ -12,37 +13,37 @@ public class TextAdventure {
 
     private static HashMap<User, TextAdventure> adventures = new HashMap<User, TextAdventure>();
 
-    public static TextAdventure getAdventure(User u, Note n){
+    public static TextAdventure getAdventure(User u, Note n) {
         System.out.println(u.getUsername() + " requested a Text Adventure");
-        if (adventures.containsKey(u)){
+        if (adventures.containsKey(u)) {
             return adventures.get(u);
-        }else{
+        } else {
             return new TextAdventure(u, n);
         }
     }
 
     private Random random;
 
-    protected enum STATE{
+    protected enum STATE {
         WORKING, WAITING, INTERACTING, RESPONSE_REQUIRED, WAITING_FOR_NAME;
     }
 
-    protected enum LOCATION{
+    protected enum LOCATION {
         LAKE("Lake"), RIVER("River"), FOREST("Forest"), CLEARING("Clearing"), PLAINS("Plains"),
         MOUNTAIN("Mountain"), HILL("Hill"), HOUSE("House"), EVIL_HOUSE("House"), BEACH("Beach"),
         DESERT("Desert"), DEAD_END("Dead End");
         private String id;
 
-        public String getName(){
+        public String getName() {
             return this.id;
         }
 
-        LOCATION(String id){
+        LOCATION(String id) {
             this.id = id;
         }
     }
 
-    protected enum DIRECTION{
+    protected enum DIRECTION {
         NORTH("north"), SOUTH("south"), EAST("east"), WEST("west"), FIRSTMOVE("firstmove");
         private String name;
 
@@ -50,25 +51,25 @@ public class TextAdventure {
             return name;
         }
 
-        public static DIRECTION getFromString(String dir){
-            for (DIRECTION d : values()){
-                if (d.getName().equalsIgnoreCase(dir)){
+        public static DIRECTION getFromString(String dir) {
+            for (DIRECTION d : values()) {
+                if (d.getName().equalsIgnoreCase(dir)) {
                     return d;
                 }
             }
             return null;
         }
 
-        DIRECTION(String name){
+        DIRECTION(String name) {
             this.name = name;
         }
     }
 
-    protected enum ENEMY{
+    protected enum ENEMY {
         BOAR, PIG, CHICKEN, MONSTER, GHOST, GHOUL;
     }
 
-    protected class Area{
+    protected class Area {
         private LOCATION locationType;
         private int id;
         private boolean newLocation = true;
@@ -117,60 +118,60 @@ public class TextAdventure {
 
         private DIRECTION prevDirect;
 
-        public Area(LOCATION location, DIRECTION prevDirection){
+        public Area(LOCATION location, DIRECTION prevDirection) {
             this.locationType = location;
             this.prevDirect = prevDirection;
             initate();
         }
 
-        public Area(DIRECTION prevDirection, Area previousArea){
+        public Area(DIRECTION prevDirection, Area previousArea) {
             this.locationType = LOCATION.values()[random.nextInt(LOCATION.values().length)];
             this.prevDirect = prevDirection;
             this.connectedArea = previousArea;
-            if (prevDirection == DIRECTION.NORTH){
+            if (prevDirection == DIRECTION.NORTH) {
                 setAreaSouth(previousArea);
             }
-            if (prevDirection == DIRECTION.SOUTH){
+            if (prevDirection == DIRECTION.SOUTH) {
                 setAreaNorth(previousArea);
             }
-            if (prevDirection == DIRECTION.EAST){
+            if (prevDirection == DIRECTION.EAST) {
                 setAreaWest(previousArea);
             }
-            if (prevDirection == DIRECTION.WEST){
+            if (prevDirection == DIRECTION.WEST) {
                 setAreaEast(previousArea);
             }
 
             initate();
         }
 
-        public Area(LOCATION locationType, DIRECTION prevDirection, Area previousArea){
+        public Area(LOCATION locationType, DIRECTION prevDirection, Area previousArea) {
             this.locationType = locationType;
             this.prevDirect = prevDirection;
             this.connectedArea = previousArea;
-            if (prevDirection == DIRECTION.NORTH){
+            if (prevDirection == DIRECTION.NORTH) {
                 setAreaSouth(previousArea);
             }
-            if (prevDirection == DIRECTION.SOUTH){
+            if (prevDirection == DIRECTION.SOUTH) {
                 setAreaNorth(previousArea);
             }
-            if (prevDirection == DIRECTION.EAST){
+            if (prevDirection == DIRECTION.EAST) {
                 setAreaWest(previousArea);
             }
-            if (prevDirection == DIRECTION.WEST){
+            if (prevDirection == DIRECTION.WEST) {
                 setAreaEast(previousArea);
             }
 
             initate();
         }
 
-        public Area(DIRECTION prevDirection){
+        public Area(DIRECTION prevDirection) {
             this.locationType = LOCATION.values()[random.nextInt(LOCATION.values().length)];
             this.prevDirect = prevDirection;
             initate();
         }
 
-        private void initate(){
-            if (prevDirect == DIRECTION.FIRSTMOVE){
+        private void initate() {
+            if (prevDirect == DIRECTION.FIRSTMOVE) {
                 setAreaNorth(new Area(DIRECTION.NORTH, this));
                 setAreaEast(new Area(DIRECTION.EAST, this));
                 setAreaSouth(new Area(DIRECTION.SOUTH, this));
@@ -178,23 +179,23 @@ public class TextAdventure {
             }
         }
 
-        public boolean canMoveNorth(){
+        public boolean canMoveNorth() {
             return getAreaNorth() != null;
         }
 
-        public boolean canMoveEast(){
+        public boolean canMoveEast() {
             return getAreaEast() != null;
         }
 
-        public boolean canMoveWest(){
+        public boolean canMoveWest() {
             return getAreaWest() != null;
         }
 
-        public boolean canMoveSouth(){
+        public boolean canMoveSouth() {
             return getAreaSouth() != null;
         }
 
-        public LOCATION getType(){
+        public LOCATION getType() {
             return this.locationType;
         }
 
@@ -216,7 +217,7 @@ public class TextAdventure {
 
     private Area currentArea, startArea;
 
-    public TextAdventure(User u, Note note){
+    public TextAdventure(User u, Note note) {
         adventures.put(u, this);
         this.user = u;
         this.starttime = System.currentTimeMillis();
@@ -235,55 +236,55 @@ public class TextAdventure {
                 "\n ➜ *Example: `_adventure " + u.getUsername() + " the Great`*");
     }
 
-    public void logAction(String action){
-        actionList.add("[" +  new SimpleDateFormat("HH:mm:ss.SSS").format(new Date(System.currentTimeMillis())) + "] " + action);
-        System.out.println("[" +  new SimpleDateFormat("HH:mm:ss.SSS").format(new Date(System.currentTimeMillis())) + "] " + action);
+    public void logAction(String action) {
+        actionList.add("[" + new SimpleDateFormat("HH:mm:ss.SSS").format(new Date(System.currentTimeMillis())) + "] " + action);
+        System.out.println("[" + new SimpleDateFormat("HH:mm:ss.SSS").format(new Date(System.currentTimeMillis())) + "] " + action);
     }
 
-    public void sendMessage(Note n, String message){
+    public void sendMessage(Note n, String message) {
         this.lastMessage = message;
         lastSentMessage = n.reply("\n" + message);
     }
 
-    public void sendInformativeMessage(Note n, String message){
+    public void sendInformativeMessage(Note n, String message) {
         lastSentMessage = n.reply("\n" + message);
     }
 
-    public void sendLastMessage(Note n, String extra){
+    public void sendLastMessage(Note n, String extra) {
         n.reply(extra + "\n" + lastMessage);
     }
 
-    private String getNewLocationText(Area originArea, LOCATION locationType, String action){
+    private String getNewLocationText(Area originArea, LOCATION locationType, String action) {
 
         String r = String.format("*We find our hero, %s %s a %s*\n** Available Directions: **\n       <north>\n" +
                 "<west>      <east>\n" +
                 "       <south>" +
                 "\n :bulb: `Use the _adventure command to go a certain direction! Example: _adventure North`", this.heroName, action, locationType.getName());
-        if (originArea.canMoveNorth()){
+        if (originArea.canMoveNorth()) {
             r = r.replaceAll("<north>", ":arrow_up:");
-        }else{
+        } else {
             r = r.replaceAll("<north>", ":negative_squared_cross_mark:");
         }
-        if (originArea.canMoveSouth()){
+        if (originArea.canMoveSouth()) {
             r = r.replaceAll("<south>", ":arrow_down:");
-        }else{
+        } else {
             r = r.replaceAll("<south>", ":negative_squared_cross_mark:");
         }
-        if (originArea.canMoveEast()){
+        if (originArea.canMoveEast()) {
             r = r.replaceAll("<east>", ":arrow_right:");
-        }else{
+        } else {
             r = r.replaceAll("<east>", ":negative_squared_cross_mark:");
         }
-        if (originArea.canMoveWest()){
+        if (originArea.canMoveWest()) {
             r = r.replaceAll("<west>", ":arrow_left:");
-        }else{
+        } else {
             r = r.replaceAll("<west>", ":negative_squared_cross_mark:");
         }
 
         return r;
     }
 
-    public void parseResponse(Note n, String response){
+    public void parseResponse(Note n, String response) {
         System.out.println("Got response for " + user.getUsername() + "'s adventure: \n" + response);
         if (stateRelation.equalsIgnoreCase("waitname") && this.state == STATE.WAITING_FOR_NAME) {
             setHeroName(response);
@@ -295,12 +296,12 @@ public class TextAdventure {
             startArea = newArea(DIRECTION.FIRSTMOVE);
             currentArea = startArea;
             sendMessage(n, getNewLocationText(currentArea, startArea.getType(), actions[random.nextInt(actions.length)])); // First Location allows for any direction of movement.
-        }else{
-            sendInformativeMessage(n, "I'm unsure of what you meant by `"+ response+"`. Type `_adventure help` to bring up the Help Menu."); // Placeholder until I add the moving system.
+        } else {
+            sendInformativeMessage(n, "I'm unsure of what you meant by `" + response + "`. Type `_adventure help` to bring up the Help Menu."); // Placeholder until I add the moving system.
         }
     }
 
-    public void setHeroName(String heroName){
+    public void setHeroName(String heroName) {
         this.heroName = heroName.replaceAll("`", "").replaceAll("_", "").replaceAll("\\*", "");
     }
 
@@ -312,16 +313,15 @@ public class TextAdventure {
         return actionList;
     }
 
-    private Area newArea(DIRECTION direction, Area currentLocation){
+    private Area newArea(DIRECTION direction, Area currentLocation) {
         Area a = new Area(direction, currentLocation);
         areas.add(a);
         return a;
     }
 
-    private Area newArea(DIRECTION direction){
+    private Area newArea(DIRECTION direction) {
         Area a = new Area(direction);
         areas.add(a);
         return a;
     }
-
 }
