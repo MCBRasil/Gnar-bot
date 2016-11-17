6f6840d1585764fb142b5ca44096509191ac714d
Gatt <zacharygatt@me.com>
2016-11-17 11:07:05 +1100

    Moved enums to seperate files

--- src/main/java/xyz/gnarbot/gnar/textadventure/TextAdventure.java
+++ src/main/java/xyz/gnarbot/gnar/textadventure/TextAdventure.java
@@ -2,6 +2,8 @@ package xyz.gnarbot.gnar.textadventure;
 
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.User;
+import xyz.gnarbot.gnar.textadventure.enums.DIRECTION;
+import xyz.gnarbot.gnar.textadventure.enums.LOCATION;
 import xyz.gnarbot.gnar.textadventure.events.FirstBagEvent;
 import xyz.gnarbot.gnar.textadventure.events.FirstSwordEvent;
 import xyz.gnarbot.gnar.textadventure.events.FirstSwordLewdEvent;
@@ -30,320 +32,6 @@ public class TextAdventure {
         WORKING, WAITING, INTERACTING, RESPONSE_REQUIRED, WAITING_FOR_NAME;
     }
 
-    protected enum LOCATION {
-        LAKE("Lake"), RIVER("River"), FOREST("Forest"), CLEARING("Clearing"), PLAINS("Plains"),
-        MOUNTAIN("Mountain"), HILL("Hill"), HOUSE("House"), EVIL_HOUSE("House?"), BEACH("Beach"),
-        DESERT("Desert"), DEAD_END("Dead End");
-        private String id;
-
-        public String getName() {
-            return this.id;
-        }
-
-        LOCATION(String id) {
-            this.id = id;
-        }
-    }
-
-    protected enum DIRECTION {
-        NORTH("north", "up"), SOUTH("south", "down"), EAST("east", "right"), WEST("west", "left"), FIRSTMOVE("firstmove", "firstmove");
-        private String name;
-        private String alt;
-
-        public String getName() {
-            return name;
-        }
-
-        public String getAlt() {
-            return alt;
-        }
-
-        public static DIRECTION getFromString(String dir) {
-            for (DIRECTION d : values()) {
-                if (d.getName().equalsIgnoreCase(dir) || d.getAlt().equalsIgnoreCase(dir)) {
-                    return d;
-                }
-            }
-            return null;
-        }
-
-        DIRECTION(String name, String alt) {
-            this.name = name;
-            this.alt = alt;
-        }
-    }
-
-    protected enum ENEMY {
-        BOAR, PIG, CHICKEN, MONSTER, GHOST, GHOUL;
-    }
-
-    private class Area {
-        private LOCATION locationType;
-        private int id;
-        private boolean newLocation = true;
-
-        private Area areaNorth, areaSouth, areaEast, areaWest, connectedArea;
-        private boolean canMoveNorth = true, canMoveSouth = true, canMoveEast = true, canMoveWest = true, hasEvent = false;
-
-        private Event relatedEvent;
-
-        public Event getRelatedEvent() {
-            return relatedEvent;
-        }
-
-        public boolean isNewLocation() {
-            return newLocation;
-        }
-
-        public boolean moveToThis(){
-            if (this.newLocation) {
-                this.newLocation = false;
-                System.out.println("New location. ID: " + areasFound);
-                if (areasFound == 4){
-                    hasEvent = true;
-                    relatedEvent = new FirstBagEvent();
-                    System.out.println("Created an Event!");
-                }
-                if (areasFound > 6){
-                    if (random.nextInt()*100 > 80){
-                        hasEvent = true;
-                        if (getHeroName().toLowerCase().contains("lewd") || getHeroName().toLowerCase().contains("mae")){
-                            relatedEvent = new FirstSwordLewdEvent();
-                        }else {
-                            relatedEvent = new FirstSwordEvent();
-                        }
-                    }
-                }
-                return true;
-            }else{
-                return false;
-            }
-        }
-
-        public Area getAreaFromDir(DIRECTION dir){
-            if (dir == DIRECTION.NORTH){
-                return getAreaNorth();
-            }
-            if (dir == DIRECTION.SOUTH){
-                return getAreaSouth();
-            }
-            if (dir == DIRECTION.EAST){
-                return getAreaEast();
-            }
-            if (dir == DIRECTION.WEST){
-                return getAreaWest();
-            }
-            return null;
-        }
-
-        public boolean canMoveInDir(DIRECTION dir){
-            if (dir == DIRECTION.NORTH){
-                return canMoveNorth();
-            }
-            if (dir == DIRECTION.SOUTH){
-                return canMoveSouth();
-            }
-            if (dir == DIRECTION.EAST){
-                return canMoveEast();
-            }
-            if (dir == DIRECTION.WEST){
-                return canMoveWest();
-            }
-            return false;
-        }
-
-        public boolean setAreaInDir(DIRECTION dir, Area area){
-            if (dir == DIRECTION.NORTH){
-                setAreaNorth(area);
-                return true;
-            }
-            if (dir == DIRECTION.SOUTH){
-                setAreaSouth(area);
-                return true;
-            }
-            if (dir == DIRECTION.EAST){
-                setAreaEast(area);
-                return true;
-            }
-            if (dir == DIRECTION.WEST){
-                setAreaWest(area);
-                return true;
-            }
-            return false;
-        }
-
-	    public boolean hasEvent() {
-		    return hasEvent;
-	    }
-
-	    public boolean hasCompletedEvent() {
-		    return (relatedEvent != null) ? relatedEvent.hasCompletedEvent() : true;
-	    }
-
-	    public Area getAreaNorth() {
-            return areaNorth;
-        }
-
-        public void setAreaNorth(Area areaNorth) {
-            this.areaNorth = areaNorth;
-        }
-
-        public Area getAreaSouth() {
-            return areaSouth;
-        }
-
-        public void setAreaSouth(Area areaSouth) {
-            this.areaSouth = areaSouth;
-        }
-
-        public Area getAreaEast() {
-            return areaEast;
-        }
-
-        public void setAreaEast(Area areaEast) {
-            this.areaEast = areaEast;
-        }
-
-        public Area getAreaWest() {
-            return areaWest;
-        }
-
-        public void setAreaWest(Area areaWest) {
-            this.areaWest = areaWest;
-        }
-
-        public Area getConnectedArea() {
-            return connectedArea;
-        }
-
-        public void setConnectedArea(Area connectedArea) {
-            this.connectedArea = connectedArea;
-        }
-
-        private DIRECTION prevDirect;
-
-        public Area(LOCATION location, DIRECTION prevDirection) {
-            this.locationType = location;
-            this.prevDirect = prevDirection;
-            initate();
-        }
-
-        public Area(DIRECTION prevDirection, Area previousArea) {
-	        if (random.nextInt() * 100 > 90) {
-		        this.locationType = LOCATION.values()[random.nextInt(LOCATION.values().length)];
-	        }else{
-		        this.locationType = previousArea.locationType;
-	        }
-            this.prevDirect = prevDirection;
-            this.connectedArea = previousArea;
-            if (prevDirection == DIRECTION.NORTH) {
-                setAreaSouth(previousArea);
-            }
-            if (prevDirection == DIRECTION.SOUTH) {
-                setAreaNorth(previousArea);
-            }
-            if (prevDirection == DIRECTION.EAST) {
-                setAreaWest(previousArea);
-            }
-            if (prevDirection == DIRECTION.WEST) {
-                setAreaEast(previousArea);
-            }
-
-            initate();
-        }
-
-        public Area(LOCATION locationType, DIRECTION prevDirection, Area previousArea) {
-            this.locationType = locationType;
-            this.prevDirect = prevDirection;
-            this.connectedArea = previousArea;
-            if (prevDirection == DIRECTION.NORTH) {
-                setAreaSouth(previousArea);
-            }
-            if (prevDirection == DIRECTION.SOUTH) {
-                setAreaNorth(previousArea);
-            }
-            if (prevDirection == DIRECTION.EAST) {
-                setAreaWest(previousArea);
-            }
-            if (prevDirection == DIRECTION.WEST) {
-                setAreaEast(previousArea);
-            }
-
-            initate();
-        }
-
-        public Area(DIRECTION prevDirection) {
-            this.locationType = LOCATION.values()[random.nextInt(LOCATION.values().length)];
-            this.prevDirect = prevDirection;
-            initate();
-        }
-
-        private void initate() {
-            if (prevDirect == DIRECTION.FIRSTMOVE) {
-                setAreaNorth(new Area(DIRECTION.NORTH, this));
-                setAreaEast(new Area(DIRECTION.EAST, this));
-                setAreaSouth(new Area(DIRECTION.SOUTH, this));
-                setAreaWest(new Area(DIRECTION.WEST, this));
-                if (getType() == LOCATION.DEAD_END){
-                    this.locationType = LOCATION.CLEARING;
-                }
-            }else{
-	            if (random.nextInt() * 100 > 90){
-		            canMoveNorth = false;
-	            }
-	            if (random.nextInt() * 100 > 90){
-		            canMoveEast = false;
-	            }
-	            if (random.nextInt() * 100 > 90){
-		            canMoveWest = false;
-	            }
-	            if (random.nextInt() * 100 > 90){
-		            canMoveSouth = false;
-	            }
-
-	            if (locationType == LOCATION.DEAD_END){
-		            canMoveSouth = false;
-		            canMoveNorth = false;
-		            canMoveEast = false;
-		            canMoveWest = false;
-	            }
-
-	            if (prevDirect == DIRECTION.NORTH){
-		            canMoveSouth = true;
-	            }
-	            if (prevDirect == DIRECTION.EAST){
-		            canMoveWest = true;
-	            }
-	            if (prevDirect == DIRECTION.WEST){
-		            canMoveEast = true;
-	            }
-	            if (prevDirect == DIRECTION.SOUTH){
-		            canMoveNorth = true;
-	            }
-            }
-        }
-
-        public boolean canMoveNorth() {
-            return canMoveNorth;
-        }
-
-        public boolean canMoveSouth() {
-            return canMoveSouth;
-        }
-
-        public boolean canMoveEast() {
-            return canMoveEast;
-        }
-
-        public boolean canMoveWest() {
-            return canMoveWest;
-        }
-
-        public LOCATION getType() {
-            return this.locationType;
-        }
-    }
-
 
 
     private User user;
@@ -367,6 +55,34 @@ public class TextAdventure {
     private int areasFound = 0;
 
     private Event currentEvent;
+
+    public Random getRandom() {
+        return random;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Long getStarttime() {
+        return starttime;
+    }
+
+    public STATE getState() {
+        return state;
+    }
+
+    public String getStateRelation() {
+        return stateRelation;
+    }
+
+    public int getAreasFound() {
+        return areasFound;
+    }
+
+    public Event getCurrentEvent() {
+        return currentEvent;
+    }
 
     public Inventory getInventory() {
         return inventory;
@@ -485,7 +201,7 @@ public class TextAdventure {
                     DIRECTION dir = DIRECTION.getFromString(response.toLowerCase());
                     if (dir != null && currentArea.canMoveInDir(dir)){
                         if (currentArea.getAreaFromDir(dir) == null){
-                            currentArea.setAreaInDir(dir, new Area(dir, currentArea));
+                            currentArea.setAreaInDir(dir, new Area(this, dir, currentArea));
                         }
                         currentArea = currentArea.getAreaFromDir(dir);
                         if (currentArea.moveToThis()){
@@ -528,13 +244,13 @@ public class TextAdventure {
 
 
     private Area newArea(DIRECTION direction, Area currentLocation) {
-        Area a = new Area(direction, currentLocation);
+        Area a = new Area(this, direction, currentLocation);
         areas.add(a);
         return a;
     }
 
     private Area newArea(DIRECTION direction) {
-        Area a = new Area(direction);
+        Area a = new Area(this, direction);
         areas.add(a);
         return a;
     }
