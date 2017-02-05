306aff3f0107b332947e6f2e4e8de7dfb0028790
Avarel <avarelpm@gmail.com>
2017-02-05 12:47:09 -0600

    Updated math

--- src/main/kotlin/xyz/gnarbot/gnar/commands/executors/general/MathCommand.kt
+++ src/main/kotlin/xyz/gnarbot/gnar/commands/executors/general/MathCommand.kt
@@ -1,9 +1,12 @@
 package xyz.gnarbot.gnar.commands.executors.general
 
+import net.dv8tion.jda.core.EmbedBuilder
 import org.apache.commons.lang3.StringUtils
+import xyz.gnarbot.gnar.Bot
 import xyz.gnarbot.gnar.commands.handlers.Command
 import xyz.gnarbot.gnar.commands.handlers.CommandExecutor
 import xyz.gnarbot.gnar.utils.Note
+import xyz.gnarbot.gnar.utils.fastSplit
 import xyz.hexav.aje.ExpressionBuilder
 import java.awt.Color
 import java.util.*
@@ -11,24 +14,29 @@ import java.util.*
 @Command(aliases = arrayOf("math"), usage = "-expression", description = "Calculate fancy math expressions.")
 class MathCommand : CommandExecutor() {
     override fun execute(note: Note, args: List<String>) {
-        val _args = args
-
         if (args.isEmpty()) {
             note.replyEmbedRaw("Error", "Please provide a math expression.", Color.RED)
             return
         }
 
-        val exp = StringUtils.join(_args, " ")
-        note.replyEmbedRaw("Expression", "```$exp```")
+        val exp = StringUtils.join(args, ' ')
+        val eb = EmbedBuilder()
+
+        eb.setTitle("Math")
+        eb.addField("Expressions", "**[${exp.fastSplit(';').map(String::trim).joinToString("\n")}]()**", true)
+        eb.setColor(Bot.color)
 
         try {
             val results = ExpressionBuilder(exp)
                     .build()
                     .evalList()
 
-            note.replyEmbedRaw("Result", "```${Arrays.toString(results)}```")
+            eb.addField("Result", "**[${Arrays.toString(results)}]()**", true)
         } catch (e: RuntimeException) {
-            note.replyEmbedRaw("Error", e.message!!, Color.RED)
+            eb.addField("Error", "**[${e.message!!}]()**", true)
+            eb.setColor(Color.RED)
         }
+
+        note.channel.sendMessage(eb.build()).queue()
     }
 }
