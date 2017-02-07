7f14bdbe5f1a97bd8fcbbd7075fe31a6ecea0501
Avarel <avarelpm@gmail.com>
2017-02-06 18:39:49 -0600

    JSON API get-ready

--- src/main/kotlin/xyz/gnarbot/gnar/api/JsonAPI.kt
+++ src/main/kotlin/xyz/gnarbot/gnar/api/JsonAPI.kt
@@ -4,9 +4,15 @@ import org.json.JSONObject
 import xyz.gnarbot.gnar.Bot
 
 class JsonAPI {
-    fun get() {
+    fun get() : JSONObject {
         val jso = JSONObject()
 
         jso.put("total_shards", Bot.shards.size)
+
+        Bot.shards.forEach {
+            jso.put(it.id.toString(), true)
+        }
+
+        return jso
     }
 }
